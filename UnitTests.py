aead190f78fe3c465f30e1b48048c43eac8af724
JustinKellogg <jdk998@mst.edu>
2012-11-30 22:42:10 -0600

    added a test for eating plant, split eating tests in two

--- UnitTests.py
+++ UnitTests.py
@@ -5,25 +5,43 @@ import unittest
 import random
 import Test
 
-bob = Test.Creature(33,2,2,0,120,10,2,3,4,4,4,1,30)
+bob = Test.Creature(33,2,2,0,120,10,2,1,4,4,4,1,30)
 marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
 sally = Test.Creature(37,0,3,1,120,90,20,4,2,2,2,7,31)
+plant = Test.Plant(22,0,2,2,33,1)
 Test.grid[2][2].append(bob)
 Test.grid[2][3].append(marv)
-
+Test.grid[0][3].append(sally)
+Test.grid[0][2].append(plant)
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
         pass
+
+    #Preconditions:
+        #Sally can eat, is in range of the plant
+    #Postcondtions:
+        #Sally gains health, the plant size is decremented
+    def test_eat_a_plant(self):
+        self.assertEqual(sally.canEat,True)
+        self.assertGreater(plant.size,0)
+        rest=sally.eat(plant.x,plant.y)
+        print (rest)
+        self.assertEqual(sally.currentHealth,90+1*10)
+        self.assertEqual(plant.size,2-1)
 
     #Pre Conditions:
       #Marv can eat, Bob is in range of Marv, Bob is at low health, Marv is at low health
     #Post Conditions:
       #Marv can not eat again, Bob is killed, Marv regains some health
-    def test_attack(self):
+    def test_eat_creature(self):
+        self.assertEqual(marv.canEat,True)
         marv.eat(bob.x,bob.y)
         self.assertEqual(marv.canEat,False)
         self.assertEqual(bob.currentHealth,10-(4-1)*Test.damageMul)
         self.assertEqual(marv.currentHealth,40+4*10)
+
+
+
 
     #Pre Conditions:
       #Marv is moving to a valid locationMarv has movement left, Marv has movement left
@@ -54,7 +72,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(marv.canEat,False)
         self.assertEqual(sally.canBreed,False)
         self.assertEqual(marv.canBreed,False)
-        self.assertEqual(sally.currentHealth,90-Test.healthPerBreed)
+        self.assertEqual(sally.currentHealth,100-Test.healthPerBreed)
         self.assertEqual(marv.currentHealth,77-Test.healthPerBreed)
         self.assertEqual(len(Test.Player2.breeding),1)
 
