534d1c0bf9b6793ab5b5659b6027b48c600e4846
JustinKellogg <jdk998@mst.edu>
2012-12-03 23:19:09 -0600

    updated Unit tests.
    Will need to replace assertTrue(res) with asserEqual(res,True)

--- UnitTests.py
+++ UnitTests.py
@@ -5,23 +5,20 @@ import unittest
 import random
 import Test
 
-bob = Test.Creature(33,2,2,0,120,10,2,1,4,4,4,1,30)
-marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
-sally = Test.Creature(37,0,3,1,120,90,20,4,2,2,2,7,31)
-plant = Test.Plant(22,0,2,2,33,1)
-Test.grid[2][2].append(bob)
-Test.grid[2][3].append(marv)
-Test.grid[0][3].append(sally)
-Test.grid[0][2].append(plant)
+#bob = Test.Creature(33,2,2,0,120,10,2,1,4,4,4,1,30)
+#marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
+#sally = Test.Creature(37,0,3,1,120,90,20,4,2,2,2,7,31)
+#plant = Test.Plant(22,0,2,2,33,1)
+#Test.grid[2][2].append(bob)
+#Test.grid[2][3].append(marv)
+#Test.grid[0][3].append(sally)
+#Test.grid[0][2].append(plant)
 
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
         pass
 
-    #Preconditions:
-        #Sally can eat, is in range of the plant
-    #Postcondtions:
-        #Sally gains health, the plant size is decremented
+
 
     def test_Creature_Constructor(self):
         temp = Test.Creature(20,0,0,0,110,110,1,2,3,4,5,6,0)
@@ -49,37 +46,61 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(plant.growthRate,2)
         self.assertEqual(plant.turnsUntilGrowth,3)
 
+     #Preconditions:
+        #Sally can eat, is in range of the plant
+      #Postcondtions:
+        #Sally gains health, the plant size is decremented
     def test_eat_a_plant(self):
+
+        sally = Test.Creature(37,0,3,1,120,90,20,4,2,2,2,7,31)
+        plant = Test.Plant(22,0,2,2,33,1)
+        Test.grid[0][3].append(sally)
+        Test.grid[0][2].append(plant)
+
         self.assertEqual(sally.canEat,True)
         self.assertGreater(plant.size,0)
         res=sally.eat(plant.x,plant.y)
         self.assertTrue(res)
         self.assertEqual(sally.currentHealth,90+1*10)
         self.assertEqual(plant.size,2-1)
+        Test.grid[0][2].remove(plant)
+        Test.grid[0][3].remove(sally)
 
     #Pre Conditions:
       #Marv can eat, Bob is in range of Marv, Bob is at low health, Marv is at low health
     #Post Conditions:
       #Marv can not eat again, Bob is killed, Marv regains some health
-    def test_eat_creature(self):
-        self.assertEqual(marv.canEat,True)
-        res = marv.eat(bob.x,bob.y)
+    def test_eat_creature_to_death(self):
+        self.bob = Test.Creature(33,2,2,0,120,10,2,1,4,4,4,1,30)
+        self.bob.name = "bob"
+        self.marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
+        tempGrid = list(Test.grid)
+        Test.grid[2][2].append(self.bob)
+        Test.grid[2][3].append(self.marv)
+        self.assertTrue(self.marv.canEat)
+        res = self.marv.eat(self.bob.x,self.bob.y)
         self.assertTrue(res)
-        self.assertEqual(marv.canEat,False)
-        self.assertEqual(bob.currentHealth,10-(4-1)*Test.damageMul)
-        self.assertEqual(marv.currentHealth,40+4*10)
+        self.assertFalse(self.marv.canEat)
+        print (self.bob.currentHealth)
+        self.assertEqual(self.bob.currentHealth,10-(4-1)*Test.damageMul)
+        self.assertEqual(self.marv.currentHealth,40+4*10)
+        Test.grid[2][3].remove(self.marv)
+
 
     #Pre Conditions:
       #Marv is moving to a valid locationMarv has movement left, Marv has movement left
     #Post Conditions
       #Marv's location is updated, Marv's health is decremented
     def test_move(self):
-        self.assertEqual(Test.getObject(marv.x-1,marv.y),None)
-        self.assertGreater(marv.movementLeft,0)
-        res = marv.move(marv.x-1,marv.y)
+        self.marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
+        Test.grid[2][3].append(self.marv)
+        self.assertEqual(Test.getObject(self.marv.x-1,self.marv.y),None)
+        self.assertGreater(self.marv.movementLeft,0)
+        res = self.marv.move(self.marv.x-1,self.marv.y)
         self.assertTrue(res)
-        self.assertEqual([marv.x,marv.y],[1,3])
-        self.assertEqual(marv.currentHealth,80-Test.healthPerMove)
+        self.assertEqual([self.marv.x,self.marv.y],[1,3])
+        self.assertEqual(self.marv.currentHealth,40-Test.healthPerMove)
+        Test.grid[1][3].remove(self.marv)
 
     #Pre condition:
         #both sally and marv can breed, both sally and marv have enough health to breed
@@ -87,21 +108,45 @@ class TestSequenceFunctions(unittest.TestCase):
         #neither sally nor marv can breed or eat, sally and marv's health is decremented
         #a new creature is added to Player 2's breeding list
     def test_zbreed(self):
+        self.marv = Test.Creature(35,2,3,1,120,80,20,4,2,2,2,7,31)
+        sally = Test.Creature(37,3,3,1,120,90,20,4,2,2,2,7,31)
+        Test.grid[2][3].append(self.marv)
+        Test.grid[0][3].append(sally)
         print (sally.x,sally.y)
-        print (marv.x,marv.y)
+        print (self.marv.x,self.marv.y)
         self.assertEqual(sally.canBreed,True)
-        self.assertEqual(marv.canBreed,True)
-        self.assertGreater(marv.currentHealth,Test.healthPerBreed)
+        self.assertEqual(self.marv.canBreed,True)
+        self.assertGreater(self.marv.currentHealth,Test.healthPerBreed)
         self.assertGreater(sally.currentHealth,Test.healthPerBreed)
-        res = marv.breed(sally)
+        res = self.marv.breed(sally)
         self.assertTrue(res)
-        self.assertEqual(sally.canEat,False)
-        self.assertEqual(marv.canEat,False)
-        self.assertEqual(sally.canBreed,False)
-        self.assertEqual(marv.canBreed,False)
-        self.assertEqual(sally.currentHealth,100-Test.healthPerBreed)
-        self.assertEqual(marv.currentHealth,77-Test.healthPerBreed)
+        self.assertFalse(sally.canEat)
+        self.assertFalse(self.marv.canEat)
+        self.assertFalse(sally.canBreed)
+        self.assertFalse(self.marv.canBreed)
+        self.assertEqual(sally.currentHealth,90-Test.healthPerBreed)
+        self.assertEqual(self.marv.currentHealth,80-Test.healthPerBreed)
         self.assertEqual(len(Test.Player2.breeding),1)
+
+        #Pre Conditions:
+    #marv can eat, Bob is in range of Marv, Bob is at low health, Marv is at low health
+    #Post Conditions:
+    #Marv can not eat again, Bob is killed, Marv regains some health
+    def test_eat_zcreature(self):
+        self.bob = Test.Creature(33,2,2,0,120,40,2,1,4,4,4,1,30)
+        self.marv = Test.Creature(35,2,3,1,120,40,20,4,2,2,2,7,31)
+        Test.grid[2][2].append(self.bob)
+        Test.grid[2][3].append(self.marv)
+        self.assertTrue(self.marv.canEat)
+        print (Test.grid)
+        res = self.marv.eat(self.bob.x,self.bob.y)
+        self.assertTrue(res)
+        print(res)
+        self.assertFalse(self.marv.canEat)
+        self.assertEqual(self.bob.currentHealth,40-(4-1)*Test.damageMul)
+        self.assertEqual(self.marv.currentHealth,40)
+        Test.grid[2][3].remove(self.marv)
+        Test.grid[2][2].remove(self.bob)
 
 
 if __name__ == '__main__':
